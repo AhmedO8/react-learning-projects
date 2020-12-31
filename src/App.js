7edff1408378efdd65a19eee7f4f419b4932f863
Ahmed08 <ahmed_0sman@hotmail.com>
2020-12-31 14:24:32 +1000

    Updated with logic

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Pokecard from './Pokecard'
 import Pokedex from './Pokedex';
+import Pokegame from './Pokegame'
 
 function App() {
   return (
     <div className="App">
-      <Pokedex/>
+      <Pokegame/>
     </div>
   );
 }
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -19,6 +19,8 @@ class Pokedex extends Component {
         return (
             <div classNmae="Pokedex">
                 <h1>POkedex!</h1>
+                <p>Total experience: {this.props.exp}</p>
+                <p>{this.props.isWinner ? 'Winner' : 'Looser'}  </p>
                 <div className="Pokedex-cards">
                 {this.props.pokemon.map((p) => (
                     <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience}></Pokecard>
